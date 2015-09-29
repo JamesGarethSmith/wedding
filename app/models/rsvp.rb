24f26b9d627479e03e8907db9e958e9f9bd84d9b
jamesgarethsmith <james.gareth.smith@gmail.com>
2015-09-29 19:27:57 +0200

    latest

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,8 @@
 class Rsvp < ActiveRecord::Base
   validates :name, :email, :total_attending, presence: true
   validates :email, uniqueness: true
+
+  def to_s
+    email
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901155655) do
+ActiveRecord::Schema.define(version: 20150929170351) do
 
   create_table "accommodations", force: :cascade do |t|
     t.string   "title"
